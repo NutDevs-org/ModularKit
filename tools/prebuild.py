ce6b771dd3847c04ed03b95fa152e47621d5ea39
Powerm1nt <leonard.chabot@nuka.works>
2025-02-14 00:12:54 +0100

    Workflows::prebuild: Fixed prebuild.py xml parsing and patching

--- tools/prebuild.py
+++ tools/prebuild.py
@@ -28,16 +28,17 @@ def main() -> None:
     file_dest: str = ".buildconfig-pom.xml"
 
     xml_root = get_root(file)
-    
+
     if sys.argv[2] == "devel":
         xml_root[3].text += f"-nightly_{get_commit_id()}"
 
 
     """ Remove GPG-Sign Maven Plugin if not a release build. """
 
+
     if not sys.argv[2] == "release":
-        xml_root[13][0][4].clear()
-        xml_root[13][0].remove(xml_root[13][0][4])
+        xml_root[13][1][5].clear()
+        xml_root[13][1].remove(xml_root[13][1][5])
 
 
     save_pom(file_dest, xml_root)
